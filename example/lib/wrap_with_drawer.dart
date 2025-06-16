b7a45e8b84401986f95677c9cc5cb512006e0674
MustaqSaiyed <saiyedmscit16@gmail.com>
2025-06-16 12:29:09 +0530

    header color change

--- example/lib/wrap_with_drawer.dart
+++ example/lib/wrap_with_drawer.dart
@@ -5,13 +5,17 @@ import 'package:leapfrog_web_component/custom_drawer/custome_web_drawer.dart';
 import 'main.dart';
 
 class WrapWithDrawer extends StatelessWidget {
-  const WrapWithDrawer({super.key, required this.child, required this.pageTitle});
+  const WrapWithDrawer({
+    super.key,
+    required this.child,
+    required this.pageTitle,
+  });
   final Widget child;
   final String pageTitle;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(backgroundColor: Colors.black),
+      drawer: Drawer(backgroundColor: Colors.white),
       body: CustomDrawer(
         menuItems: menuItems,
         onMenuTap: (route) => context.push(route),
@@ -24,7 +28,7 @@ class WrapWithDrawer extends StatelessWidget {
         userLastName: "Doe",
         titleName: pageTitle,
         isSearchShow: true,
-        headerColor: Colors.blue,
+        headerColor: Colors.black,
         drawerColor: Colors.black,
         version: "1.0.0",
         titleStyle: const TextStyle(color: Colors.white, fontSize: 20),
@@ -38,11 +42,20 @@ class WrapWithDrawer extends StatelessWidget {
         //headerWidget: SizedBox.shrink(),
         drawerHeader: Column(
           children: [
-            CircleAvatar(radius: 40, backgroundImage: AssetImage('assets/user_place_holder.png')),
+            CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage('assets/user_place_holder.png'),
+            ),
             SizedBox(height: 10),
-            Text("John Doe", style: TextStyle(color: Colors.white, fontSize: 16)),
+            Text(
+              "John Doe",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
             SizedBox(height: 10),
-            Text("jH9o7@example.com", style: TextStyle(color: Colors.white, fontSize: 12)),
+            Text(
+              "jH9o7@example.com",
+              style: TextStyle(color: Colors.white, fontSize: 12),
+            ),
             SizedBox(height: 10),
           ],
         ),
